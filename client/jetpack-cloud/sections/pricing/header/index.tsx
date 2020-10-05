dfeaf8a6d2bb7485fd4983d133c6168cb170b19b
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-10-05 14:56:50 -0500

    Make title and masterbar copy reactive

--- client/jetpack-cloud/sections/pricing/header/index.tsx
+++ client/jetpack-cloud/sections/pricing/header/index.tsx
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { translate } from 'i18n-calypso';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal dependencies
@@ -16,24 +16,28 @@ import JetpackComMasterbar from '../jpcom-masterbar';
  */
 import './style.scss';
 
-const Header = () => (
-	<>
-		<JetpackComMasterbar />
-		<div className="header">
-			<FormattedHeader
-				className="header__main-title"
-				headerText={ preventWidows(
-					translate( 'Security, performance, and marketing tools for WordPress' )
-				) }
-				align="center"
-			/>
-			<p>
-				{ translate(
-					'Get everything your site needs, in one package — so you can focus on your business.'
-				) }
-			</p>
-		</div>
-	</>
-);
+const Header = () => {
+	const translate = useTranslate();
+
+	return (
+		<>
+			<JetpackComMasterbar />
+			<div className="header">
+				<FormattedHeader
+					className="header__main-title"
+					headerText={ preventWidows(
+						translate( 'Security, performance, and marketing tools for WordPress' )
+					) }
+					align="center"
+				/>
+				<p>
+					{ translate(
+						'Get everything your site needs, in one package — so you can focus on your business.'
+					) }
+				</p>
+			</div>
+		</>
+	);
+};
 
 export default Header;
