1c1aaee3a011250e1e8f8584647afef93f77c109
Robert Franklin <42627630+robertf4@users.noreply.github.com>
2020-10-07 14:13:19 -0500

    Create translated-copy.tsx

--- client/my-sites/plans-v2/utils.ts
+++ client/my-sites/plans-v2/utils.ts
@@ -4,7 +4,7 @@
 import { TranslateResult } from 'i18n-calypso';
 import { compact, isArray, isObject } from 'lodash';
 import page from 'page';
-import React, { createElement, Fragment } from 'react';
+import { createElement, Fragment } from 'react';
 
 /**
  * Internal dependencies
@@ -13,7 +13,6 @@ import { getFeatureByKey, getFeatureCategoryByKey } from 'lib/plans/features-lis
 import {
 	ALL,
 	DAILY_PLAN_TO_REALTIME_PLAN,
-	DAILY_PRODUCTS,
 	EXTERNAL_PRODUCTS_LIST,
 	EXTERNAL_PRODUCTS_SLUG_MAP,
 	FEATURED_PRODUCTS,
@@ -28,13 +27,13 @@ import {
 	PERFORMANCE,
 	PLAN_COMPARISON_PAGE,
 	PRODUCTS_WITH_OPTIONS,
-	REALTIME_PRODUCTS,
 	SECURITY,
 	SUBTYPE_TO_OPTION,
 	UPGRADEABLE_WITH_NUDGE,
 	UPSELL_PRODUCT_MATRIX,
 } from './constants';
 import RecordsDetails from './records-details';
+import { getSelectorProductCopy } from './translated-copy';
 import { addItems } from 'lib/cart/actions';
 import { jetpackProductItem } from 'lib/cart-values/cart-items';
 import isJetpackCloud from 'lib/jetpack/is-jetpack-cloud';
@@ -57,12 +56,6 @@ import {
 	JETPACK_BACKUP_PRODUCTS,
 	JETPACK_PRODUCT_PRICE_MATRIX,
 	JETPACK_SEARCH_PRODUCTS,
-	PRODUCT_JETPACK_ANTI_SPAM,
-	PRODUCT_JETPACK_ANTI_SPAM_MONTHLY,
-	PRODUCT_JETPACK_CRM,
-	PRODUCT_JETPACK_CRM_MONTHLY,
-	PRODUCT_JETPACK_SCAN,
-	PRODUCT_JETPACK_SCAN_MONTHLY,
 } from 'lib/products-values/constants';
 import {
 	Product,
@@ -78,7 +71,6 @@ import { addQueryArgs } from 'lib/route';
 import type {
 	Duration,
 	SelectorProduct,
-	SelectorProductCopy,
 	SelectorProductSlug,
 	DurationString,
 	SelectorProductFeaturesItem,
@@ -220,94 +212,6 @@ export function getProductTypeOptions( translate: Function ) {
 			label: translate( 'All' ),
 		},
 	};
-}
-
-export function getSelectorProductCopy(
-	productSlug: string,
-	translate: Function
-): SelectorProductCopy {
-	switch ( productSlug ) {
-		case PRODUCT_JETPACK_ANTI_SPAM:
-		case PRODUCT_JETPACK_ANTI_SPAM_MONTHLY:
-			return {
-				displayName: translate( 'Jetpack Anti-spam' ),
-				shortName: translate( 'Anti-spam', {
-					comment: 'Short name of Jetpack Anti-spam',
-				} ),
-				tagline: translate( 'Block spam automatically' ),
-				description: translate(
-					'Automated spam protection for comments and forms. Save time, get more responses, and give your visitors a better experience.'
-				),
-				buttonLabel: translate( 'Get Anti-spam' ),
-			};
-		case OPTIONS_JETPACK_BACKUP:
-		case OPTIONS_JETPACK_BACKUP_MONTHLY:
-			return {
-				displayName: translate( 'Jetpack Backup' ),
-				shortName: translate( 'Backup', {
-					comment: 'Short name of the Jetpack Backup generic product',
-				} ),
-				tagline: translate( 'Recommended for all sites' ),
-				description: translate(
-					'Never lose a word, image, page, or time worrying about your site.'
-				),
-				buttonLabel: translate( 'Get Backup' ),
-			};
-		case PLAN_JETPACK_COMPLETE:
-		case PLAN_JETPACK_COMPLETE_MONTHLY:
-			return {
-				displayName: translate( 'Jetpack Complete' ),
-				shortName: translate( 'Complete', {
-					comment: 'Short name of Jetpack Complete',
-				} ),
-				tagline: translate( 'For best-in-class WordPress sites' ),
-				description: translate(
-					'Superpower your site with everything Jetpack has to offer: real-time security, enhanced search, CRM, and marketing, growth, and design tools.'
-				),
-				buttonLabel: translate( 'Get Jetpack Complete' ),
-			};
-		case PRODUCT_JETPACK_CRM:
-		case PRODUCT_JETPACK_CRM_MONTHLY:
-			return {
-				displayName: translate( 'Jetpack CRM' ),
-				shortName: translate( 'CRM', {
-					comment: 'Short name of the Jetpack CRM',
-				} ),
-				tagline: translate( 'Manage contacts effortlessly' ),
-				description: translate(
-					'The most simple and powerful WordPress CRM. Improve customer relationships and increase profits.'
-				),
-				buttonLabel: translate( 'Get CRM' ),
-			};
-		case PRODUCT_JETPACK_SCAN:
-		case PRODUCT_JETPACK_SCAN_MONTHLY:
-			return {
-				displayName: translate( 'Jetpack Scan' ),
-				shortName: translate( 'Scan', {
-					comment: 'Short name of Jetpack Scan',
-				} ),
-				tagline: translate( 'Protect your site' ),
-				description: translate(
-					'Automatic scanning and one-click fixes keep your site one step ahead of security threats.'
-				),
-				buttonLabel: translate( 'Get Scan' ),
-			};
-		case OPTIONS_JETPACK_SECURITY:
-		case OPTIONS_JETPACK_SECURITY_MONTHLY:
-			return {
-				displayName: translate( 'Jetpack Security' ),
-				shortName: translate( 'Security', {
-					comment: 'Short name of the Jetpack Security generic plan',
-				} ),
-				tagline: translate( 'Comprehensive WordPress protection' ),
-				description: translate(
-					'Enjoy the peace of mind of complete site security. ' +
-						'Easy-to-use, powerful security tools guard your site, so you can focus on your business.'
-				),
-			};
-		default:
-			throw `Unknown SelectorProductSlug: ${ productSlug }`;
-	}
 }
 
 /**
@@ -718,34 +622,3 @@ export function getPathToUpsell(
 
 	return addQueryArgs( urlQueryArgs, path );
 }
-
-/**
- * Append "Available Options: Real-time and Daily" to the product description.
- *
- * @param product SelectorProduct
- * @param translate Function The translate function, ideally retrieved from useTranslate().
- *
- * @returns ReactNode | TranslateResult
- */
-export const getJetpackDescriptionWithOptions = (
-	product: SelectorProduct,
-	translate: Function
-): React.ReactNode | TranslateResult => {
-	const em = React.createElement( 'em', null, null );
-
-	const { description } = getSelectorProductCopy( product.productSlug, translate );
-
-	// If the product has 'subtypes' (containing daily and real-time product slugs).
-	// then append "Available options: Real-time or Daily" to the product description.
-	return product.subtypes.some( ( subtype ) => DAILY_PRODUCTS.includes( subtype ) ) &&
-		product.subtypes.some( ( subtype ) => REALTIME_PRODUCTS.includes( subtype ) )
-		? translate( '%(productDescription)s {{em}}Available options: Real-time or Daily.{{/em}}', {
-				args: {
-					productDescription: description,
-				},
-				components: {
-					em,
-				},
-		  } )
-		: description;
-};
